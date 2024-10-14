7b098598dd943b131591e707807fc1379f48bb72
Yan Basílio <69776644+YanFellippe@users.noreply.github.com>
2024-10-14 11:11:20 -0300

    Add files via upload

--- dba/codigos-sql/sistema_vendas.sql
+++ dba/codigos-sql/sistema_vendas.sql
@@ -35,7 +35,7 @@ CREATE TABLE categoria (
 
 CREATE TABLE produto (
 	id_produto INT AUTO_INCREMENT PRIMARY KEY,
-    codigo INT AUTO_INCREMENT PRIMARY KEY,
+    codigo INT(5),
     descricao VARCHAR(100) NOT NULL,
     preco DECIMAL(10, 2) NOT NULL,
     situacao ENUM('disponivel', 'indisponivel') DEFAULT 'disponivel',
@@ -48,7 +48,6 @@ CREATE TABLE venda (
     id_venda INT AUTO_INCREMENT PRIMARY KEY,
     valor_bruto DECIMAL(10, 2) NOT NULL,
     valor_total DECIMAL(10, 2) NOT NULL,
-    total DECIMAL(10, 2) NOT NULL,
     desconto DECIMAL(10, 2) DEFAULT 0.00,
     acrescimo DECIMAL(10, 2) DEFAULT 0.00,
     situacao ENUM('concluida', 'pendente', 'cancelada') DEFAULT 'pendente',
@@ -66,7 +65,43 @@ CREATE TABLE itens_venda (
     valor_total DECIMAL(10, 2) NOT NULL,
     cancelado BOOLEAN DEFAULT FALSE,
     id_venda INT NOT NULL,
-    codigo_produto INT NOT NULL,
-    FOREIGN KEY (id_venda) REFERENCES venda(id_venda),
-    FOREIGN KEY (codigo_produto) REFERENCES produto(codigo)
+    FOREIGN KEY (id_venda) REFERENCES venda(id_venda)
 );
+
+-- Campo dos inserts 
+
+SELECT * FROM pessoa;
+INSERT INTO pessoa(nome, idade, cpf, situacao) VALUES
+('Bora Bilson', 42, '12345678900', 'ativo'),
+('Amostradinho da Silva', 26, '00298366118', 'ativo'),
+('Inês Sistente', 52, '93877122300', 'ativo'),
+('Oreia Umida', 37, '73849987190', 'ativo'),
+('Oreia Seca', 62, '12388900876', 'ativo'),
+('Silas Cow', 19, '17389098177', 'ativo');
+
+SELECT * FROM atendente;
+INSERT INTO atendente(matricula, salario, situacao, id_pessoa) VALUES
+('A3782849012344456981', 2775.44, 'ativo', 1),
+('A7986435653268386666', 2775.44, 'ativo', 2),
+('A9872345821073892460', 2775.44, 'ativo', 3);
+
+SELECT * FROM cliente;
+INSERT INTO cliente(telefone, saldo, email, situacao, id_pessoa) VALUES
+('61999885678', 19877.19, 'testandocoins@gmail.com', 'ativo', 4),
+('61998765423', 22987.55, 'matanubi@gmail.com', 'ativo', 5),
+('61898988534', 6789.99, 'quetirofoiesse@gmail.com', 'ativo', 6);
+
+SELECT * FROM categoria;
+INSERT INTO categoria(descricao) VALUES
+('Aparelhos Eletrônicos'),
+('Aparelhos Eletrodomésticos'),
+('Itens de Cozinha');
+
+SELECT * FROM produto;
+INSERT INTO produto(codigo, descricao, preco, situacao, quantidade_estoque, id_categoria) VALUES
+(12345 , 'Iphone 16 Pro Max', 12777.99, 'disponivel', 200, 1),
+(54321 , 'Samsung Galaxy S25 Ultra', 7897.55, 'disponivel', 100, 1),
+(09876 , 'Ait Frier - Tramontina', 455.99, 'disponivel', 55, 3),
+(67890 , 'Multiprocessador - Tramontina', 199.99, 'disponivel', 22, 3),
+(10293 , 'Geladeira Duplex - Eletrolux', 3299.99, 'disponivel', 19, 2),
+(76859 , 'Kit Talheres de ouro', 1255.25, 'disponivel', 333, 2);
